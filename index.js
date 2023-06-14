cef0b1455937aadcf97c498ecdedb46fdd462539
yeasin550 <yeasin51570@gmail.com>
2023-06-14 17:48:38 +0800

    specific user instructor data send client site

--- index.js
+++ index.js
@@ -339,7 +339,7 @@ async function run() {
 
   app.patch("/users/instructor/:id", async(req, res) => {
     const id = req.params.id;
-    // console.log(id);
+   
     const filter = { _id: new ObjectId(id) };
     const updateDoc = {
       $set: {
